b76eaaed768745537b35dbe6c1438ccf31cf956f
aotoladeji <aot.oladeji@gmail.com>
2026-08-21 15:12:16 +0100

    server issues

--- api/index.js
+++ api/index.js
@@ -1,1 +1,11 @@
-module.exports = require('../backend/server');
+let app;
+
+module.exports = (request, response) => {
+	try {
+		app ||= require('../backend/server');
+		return app(request, response);
+	} catch (error) {
+		console.error('API startup failed:', error);
+		return response.status(500).json({ error: 'The API could not start. Check the Vercel function logs.' });
+	}
+};
